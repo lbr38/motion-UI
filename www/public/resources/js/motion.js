c07d04cc9a1af5aa6c4defdc58268f3423e78935
Ludovic Brun <ludovic.brun@prowebce.com>
2022-08-21 14:34:45 +0200

    2.1.8

--- www/public/resources/js/motion.js
+++ www/public/resources/js/motion.js
@@ -1,3 +1,163 @@
+$(document).ready(function () {
+    /**
+     *  Create empty motion status and events charts
+     */
+    var ctx = document.getElementById('motion-event-chart').getContext('2d');
+    var myEventChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                data: [],
+                label: "Total events per day",
+                borderColor: '#3e95cd',
+                fill: false
+            },
+            {
+                data: [],
+                label: "Total files recorded per day",
+                borderColor: '#ea974d',
+                fill: false
+            }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            tension: 0.2,
+            scales: {
+                x: {
+                    display: true,
+                },
+                y: {
+                    beginAtZero: true,
+                    display: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                }
+            },
+        }
+    });
+
+    var yLabels = {
+        0 : 'inactive',
+        1 : 'active'
+    }
+    var ctx = document.getElementById('motion-status-chart').getContext('2d');
+    var myMotionStatusChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                data: [],
+                label: "Motion service activity (48h)",
+                borderColor: '#d8524e',
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            tension: 0.2,
+            scales: {
+                x: {
+                    display: true,
+                },
+                y: {
+                    beginAtZero: true,
+                    display: true,
+                    ticks: {
+                        stepSize: 1,
+                        callback: function (value, index, values) {
+                            return yLabels[value];
+                        }
+                    }
+                }
+            },
+        }
+    });
+});
+
+/**
+ *  Inject charts labels and data
+ */
+loadAllStatsCharts();
+
+/**
+ *  Load and generate stats charts
+ */
+function loadAllStatsCharts()
+{
+    /**
+     *  Get labels and data
+     */
+    var eventLabels = $('#motion-event-chart-labels-data').attr('labels').split(", ");
+    var eventData = $('#motion-event-chart-labels-data').attr('event-data').split(", ");
+    var filesData = $('#motion-event-chart-labels-data').attr('files-data').split(", ");
+
+    var statusLabels = $('#motion-status-chart-labels-data').attr('labels').split(", ");
+    var statusData = $('#motion-status-chart-labels-data').attr('status-data').split(", ");
+
+    /**
+     *  Inject/update labels and data into the charts
+     */
+    myEventChart.data.labels = eventLabels;
+    myEventChart.data.datasets[0].data = eventData;
+    myEventChart.data.datasets[1].data = filesData;
+    myEventChart.update();
+
+    myMotionStatusChart.data.labels = statusLabels;
+    myMotionStatusChart.data.datasets[0].data = statusData;
+    myMotionStatusChart.update();
+}
+
+/**
+ * Function: print stats between selected dates
+ * @param {*} dateStart
+ * @param {*} dateEnd
+ */
+function statsDateSelect(dateStart, dateEnd)
+{
+    /**
+     *  Add specified dates into cookies
+     */
+    document.cookie = "statsDateStart="+dateStart;
+    document.cookie = "statsDateEnd="+dateEnd;
+
+    /**
+     *  Then reload stats div
+     */
+    reloadContentById('motion-stats-labels-data');
+
+    /**
+     *  Wait for the div reload, then reload charts
+     */
+    setTimeout(function () {
+        loadAllStatsCharts();
+    }, 500);
+}
+
+/**
+ * Function: print events between selected dates
+ * @param {*} dateStart
+ * @param {*} dateEnd
+ */
+function eventDateSelect(dateStart, dateEnd)
+{
+    /**
+     *  Add specified dates into cookies
+     */
+    document.cookie = "eventDateStart="+dateStart;
+    document.cookie = "eventDateEnd="+dateEnd;
+
+    /**
+     *  Then reload events div
+     */
+    reloadContentById('events-captures-div');
+}
+
 /**
  *  Event: Start motion capture
  */
@@ -70,6 +230,34 @@ $(document).on('click','#configure-alerts-btn',function () {
  */
 $(document).on('click','#how-to-alert-btn',function () {
     $("#how-to-alert-container").slideToggle('100');
+});
+
+/**
+ *  Event: validate select event dates form
+ */
+$(document).on('submit','#statsDateForm',function () {
+    event.preventDefault();
+
+    var dateStart = $(this).find('input[type=date][name="dateStart"]').val();
+    var dateEnd = $(this).find('input[type=date][name="dateEnd"]').val();
+
+    statsDateSelect(dateStart, dateEnd);
+
+    return false;
+});
+
+/**
+ *  Event: validate select event dates form
+ */
+$(document).on('submit','#eventDateForm',function () {
+    event.preventDefault();
+
+    var dateStart = $(this).find('input[type=date][name="dateStart"]').val();
+    var dateEnd = $(this).find('input[type=date][name="dateEnd"]').val();
+
+    eventDateSelect(dateStart, dateEnd);
+
+    return false;
 });
 
 /**
@@ -184,7 +372,7 @@ $(document).on('submit','#autostart-conf-form',function () {
 });
 
 /**
- *  Add a new device
+ *  Event: Add a new device
  */
 $(document).on('submit','#device-presence-form',function () {
     event.preventDefault();
